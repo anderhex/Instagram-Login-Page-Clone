3123434515c83def6aa91c988e46ffb240add5ba
AnderHex <103506080+anderhex@users.noreply.github.com>
2022-06-15 15:36:07 -0300

    Instagram Login Page Clone

---
+++ assets/js/index.js
@@ -0,0 +1,33 @@
+/*
+   Instagram Login Page Clone
+   @Author: Anderson Daniel
+   @Github: Anderhex
+   @License: MIT
+*/
+const instaPhoneSlider = images => {
+
+    let screen = document.getElementById('screen');
+    let screenIndex = 0;
+    setInterval(() => {
+        screenIndex = screenIndex >= images.length - 1 ? 0 : screenIndex + 1;
+        console.log('Index: ' + screenIndex, 'Image length:' + images.length);
+        screen.animate([
+            { opacity: 0 },
+            { opacity: 1 }
+        ], { iterations: 1, duration: 1000 });
+        screen.src = images[screenIndex];
+
+    }, 3000);
+
+
+}
+
+instaPhoneSlider(
+    [
+        "assets/img/screen01.png",
+        "assets/img/screen02.png",
+        "assets/img/screen03.png",
+        "assets/img/screen04.png",
+        "assets/img/screen05.png"
+    ]
+)
